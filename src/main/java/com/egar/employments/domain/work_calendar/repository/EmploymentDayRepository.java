a190896695a11b777fb73a5534228cf626d6c75f
Andrey Golikov <z-a-t-o@yandex.ru>
2022-09-20 12:05:15 +0300

    refactor repositories

--- src/main/java/com/egar/employments/domain/work_calendar/repository/EmploymentDayRepository.java
+++ src/main/java/com/egar/employments/domain/work_calendar/repository/EmploymentDayRepository.java
@@ -1,8 +1,15 @@
 package com.egar.employments.domain.work_calendar.repository;
 
 import com.egar.employments.domain.work_calendar.entity.EmploymentDay;
-import com.egar.employments.domain.work_calendar.entity.WorkDay;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface EmploymentDayRepository extends JpaRepository<EmploymentDay, Long> {
+
+    @Query("select ed from EmploymentDay ed where lower(ed.projectName) like lower(concat('%', ?1,'%')) and ed.date between ?2 and ?3")
+    List<EmploymentDay> findEmploymentDay(String projectName, LocalDate startDate, LocalDate endDate);
+
 }
--- src/main/java/com/egar/employments/domain/work_calendar/repository/WorkDayRepository.java
+++ src/main/java/com/egar/employments/domain/work_calendar/repository/WorkDayRepository.java
@@ -1,11 +1,11 @@
 package com.egar.employments.domain.work_calendar.repository;
 
-import com.egar.employments.domain.work_calendar.entity.WorkDay;
+import com.egar.employments.domain.work_calendar.entity.WeekendAndShortDays;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public interface WorkDayRepository extends JpaRepository<WorkDay, Long> {
-    List<WorkDay> findWorkDayByDateBetween(LocalDate startDate, LocalDate endDate);
+public interface WorkDayRepository extends JpaRepository<WeekendAndShortDays, Long> {
+    List<WeekendAndShortDays> findWeekendAndShortDaysByDateBetween(LocalDate startDate, LocalDate endDate);
 }
