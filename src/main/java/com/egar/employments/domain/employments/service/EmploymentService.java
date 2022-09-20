60d335dacf5b463971d5360dfefa2c624bbba0d3
Andrey Golikov <z-a-t-o@yandex.ru>
2022-09-20 12:10:08 +0300

    refactor EmploymentService

--- src/main/java/com/egar/employments/domain/employments/service/EmploymentService.java
+++ src/main/java/com/egar/employments/domain/employments/service/EmploymentService.java
@@ -1,33 +1,24 @@
 package com.egar.employments.domain.employments.service;
 
-import com.egar.employments.domain.employments.EmploymentsDto;
+import com.egar.employments.domain.employments.dto.EmploymentsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.egartech.sdk.api.TaskClient;
-import ru.egartech.sdk.dto.task.TaskDto;
-import ru.egartech.sdk.dto.task.customfield.field.label.LabelOptionDto;
-import ru.egartech.sdk.dto.task.customfield.field.label.LabelsFieldDto;
-import ru.egartech.sdk.dto.task.customfield.field.relationship.RelationshipFieldDto;
-import ru.egartech.sdk.dto.task.customfield.field.text.TextFieldDto;
+import ru.egartech.sdk.dto.task.deserialization.TaskDto;
+import ru.egartech.sdk.dto.task.deserialization.customfield.field.label.LabelOptionDto;
+import ru.egartech.sdk.dto.task.deserialization.customfield.field.label.LabelsFieldDto;
+import ru.egartech.sdk.dto.task.deserialization.customfield.field.text.TextFieldDto;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class EmploymentsService {
+public class EmploymentService {
 
     private final TaskClient taskClient;
-
-//    @Value("${lists.developers}")
-//    private Integer devlistId;
-
-//    @Value("${fields.egar_id}")
-//    private String egarIdFieldId;
-
-//    @Value("${fields.employments}")
-//    private String employmentsFieldId;
 
     @Value("${fields.start_date}")
     private String startDateFieldId;
@@ -40,12 +31,15 @@ public class EmploymentsService {
      * @param taskId - id таски где находится информация по занятости
      * @return объект занятости(название проекта, дата начала)
      */
-    private EmploymentsDto getTaskById(String taskId) {
+    private Optional<EmploymentsDto> getTaskById(String taskId) {
         TaskDto taskDto = taskClient.getTaskById(taskId, false);
         List<LabelOptionDto> labelOptionDtoList = taskDto.customField(projectsFieldId, LabelsFieldDto.class).getValue();
         String projectName = labelOptionDtoList.get(0).getLabel();
         String startEmploymentDate = taskDto.customField(startDateFieldId, TextFieldDto.class).getValue();
-        return new EmploymentsDto(projectName, startEmploymentDate);
+        if (projectName!= null & startEmploymentDate != null) {
+            return Optional.of(new EmploymentsDto(projectName, startEmploymentDate));
+        }
+        return Optional.empty();
     }
 
     /**
@@ -54,7 +48,11 @@ public class EmploymentsService {
      * @return список объектов занятости(название проекта, дата начала)
      */
     public List<EmploymentsDto> getEmployments(List<String> taskIds) {
-        List<EmploymentsDto> employments = taskIds.stream().map(this::getTaskById).collect(Collectors.toList());
+        List<EmploymentsDto> employments = taskIds.stream()
+                .map(this::getTaskById)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
         return employments;
     }
 }
