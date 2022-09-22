7d049680140e917c1948101c4bd4337b06d47cdc
Andrey Golikov <z-a-t-o@yandex.ru>
2022-09-22 16:31:25 +0300

    test

---
+++ src/test/java/com/egar/employments/domain/employments/service/EmploymentServiceTest.java
@@ -0,0 +1,9 @@
+package com.egar.employments.domain.employments.service;
+
+import com.egar.employments.AbstractSpringBootTest;
+
+class EmploymentServiceTest extends AbstractSpringBootTest {
+
+
+
+}
