954e36c3dd5938cb8650e0c56928fa6d5b97c853
Andrey Golikov <z-a-t-o@yandex.ru>
2022-09-29 17:50:16 +0300

    refactor

--- src/main/java/ru/egar/employments/domain/employments/service/EmploymentsServiceImpl.java
+++ src/main/java/ru/egar/employments/domain/employments/service/EmploymentsServiceImpl.java
@@ -3,7 +3,10 @@ package ru.egar.employments.domain.employments.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.egar.employments.domain.work_calendar.service.EmploymentCalendarService;
+import ru.egar.employments.model.EmploymentCalendarDto;
 import ru.egar.employments.model.EmploymentDto;
+import ru.egar.employments.service.EmploymentsService;
 import ru.egartech.sdk.api.TaskClient;
 import ru.egartech.sdk.dto.task.deserialization.TaskDto;
 import ru.egartech.sdk.dto.task.deserialization.customfield.field.label.LabelOptionDto;
@@ -14,14 +17,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-/**
- * сервис для получения списка проектов, на которых задействован сотрудник
- */
+
 @Service
 @RequiredArgsConstructor
-public class EmploymentService {
+public class EmploymentsServiceImpl implements EmploymentsService {
 
     private final TaskClient taskClient;
+
+    private final EmploymentCalendarService employmentCalendarService;
 
     @Value("${fields.start_date.id}")
     private String startDateFieldId;
@@ -33,21 +36,8 @@ public class EmploymentService {
     private String projectsFieldId;
 
     /**
-     * метод получения списка объектов занятости по списку id тасок
-     * @param taskIds - список id тасок
-     * @return список объектов занятости(название проекта, дата начала)
-     */
-    public List<EmploymentDto> getEmployments(List<String> taskIds) {
-        List<EmploymentDto> employments = taskIds.stream()
-                .map(this::getEmploymentsByTaskId)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toList());
-        return employments;
-    }
-
-    /**
      * метод получения информации о занятости(название проекта, дата начала) по одному id таски
+     *
      * @param taskId - id таски где находится информация по занятости
      * @return объект занятости(название проекта, дата начала)
      */
@@ -57,9 +47,32 @@ public class EmploymentService {
         String projectName = labelOptionDtoList.get(0).getLabel();
         String startEmploymentDate = taskDto.customField(startDateFieldId, TextFieldDto.class).getValue();
         String endEmploymentDate = taskDto.customField(endDateFieldId, TextFieldDto.class).getValue();
-        if (projectName!= null & startEmploymentDate != null & endEmploymentDate == null) {
+        if (projectName != null & startEmploymentDate != null & endEmploymentDate == null) {
             return Optional.of(new EmploymentDto(projectName, startEmploymentDate));
         }
         return Optional.empty();
+    }
+
+    @Override
+    public EmploymentCalendarDto getEmploymentCalendar(String projectName,
+                                                       String beginDate,
+                                                       String egarId,
+                                                       String profileListId) {
+        return employmentCalendarService.getEmploymentCalendar(projectName, beginDate, egarId, profileListId);
+    }
+
+    /**
+     * метод получения списка объектов занятости по списку id тасок
+     *
+     * @param id - список id тасок
+     * @return список объектов занятости(название проекта, дата начала)
+     */
+    @Override
+    public List<EmploymentDto> getEmploymentsByIds(List<String> id) {
+        return id.stream()
+                .map(this::getEmploymentsByTaskId)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
     }
 }
