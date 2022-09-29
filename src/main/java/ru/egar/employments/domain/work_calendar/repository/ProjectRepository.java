bd5190e38f47489655d96965d12f4e6589c8fa4c
Andrey Golikov <z-a-t-o@yandex.ru>
2022-09-29 16:03:54 +0300

    remove repository

--- src/main/java/ru/egar/employments/domain/work_calendar/repository/ProjectRepository.java
+++ src/main/java/ru/egar/employments/domain/work_calendar/repository/ProjectRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.egar.employments.domain.work_calendar.entity.Project;
 
-@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Project findByProjectName(String name);
 }
