514baa6fd0c5018ca905df74ffb0041422e3bef5
Andrey Golikov <z-a-t-o@yandex.ru>
2022-10-05 11:25:11 +0300

    remove unused imports

--- src/main/java/ru/egar/employments/domain/work_calendar/repository/ProjectRepository.java
+++ src/main/java/ru/egar/employments/domain/work_calendar/repository/ProjectRepository.java
@@ -1,7 +1,6 @@
 package ru.egar.employments.domain.work_calendar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.egar.employments.domain.work_calendar.entity.Project;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
