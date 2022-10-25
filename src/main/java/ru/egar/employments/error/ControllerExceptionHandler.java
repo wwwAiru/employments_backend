8fb0f9ada5552631f17535897abadd0ef206b353
Andrey Golikov <z-a-t-o@yandex.ru>
2022-10-25 15:15:32 +0300

    add ControllerExceptionHandler

---
+++ src/main/java/ru/egar/employments/error/ControllerExceptionHandler.java
@@ -0,0 +1,38 @@
+package ru.egar.employments.error;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import ru.egar.employments.error.exception.VacationsCantReceivedException;
+import ru.egartech.sdk.exception.dto.ApiErrorDto;
+import ru.egartech.sdk.exception.handler.AbstractRestExceptionHandler;
+import ru.egartech.sdk.util.MessageSourceUtils;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class ControllerExceptionHandler extends AbstractRestExceptionHandler {
+    private final MessageSourceUtils messageSource;
+
+    public ControllerExceptionHandler(MessageSourceUtils messageSourceUtils) {
+        super(messageSourceUtils);
+        this.messageSource = messageSourceUtils;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ApiErrorDto handleMissedException(RuntimeException exception, WebRequest webRequest) {
+        exception.printStackTrace();
+        return buildMessage(messageSource, exception, webRequest, "unknownError", exception.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(VacationsCantReceivedException.class)
+    protected ApiErrorDto handleNotAvailableException(VacationsCantReceivedException exception, WebRequest webRequest) {
+        exception.printStackTrace();
+        return buildMessage(messageSource, exception, webRequest, "vacationsServiceError");
+    }
+
+}
