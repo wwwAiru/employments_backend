963abc7081bc8fd315fdd8a0473bc989c64d5f62
Andrey Golikov <z-a-t-o@yandex.ru>
2022-10-26 13:55:58 +0300

    refactor ResponseEntity<VacationPeriodDto[]> -> ResponseEntity<List<VacationPeriodDto>>

--- src/main/java/ru/egar/employments/domain/vacations/repository/VacationClient.java
+++ src/main/java/ru/egar/employments/domain/vacations/repository/VacationClient.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.egar.employments.domain.vacations.dto.VacationPeriodDto;
-import ru.egar.employments.error.exception.VacationsCantReceivedException;
+import ru.egar.employments.error.exception.VacationsReceiveException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,16 +30,16 @@ public class VacationClient {
     private String url;
 
     public List<VacationPeriodDto> getVacations(String egarId, String profileListId) {
-        Map<String, String> params = new HashMap<>();
         List<VacationPeriodDto> vacationPeriods;
+        Map<String, String> params = new HashMap<>();
         params.put("egarId", egarId);
         params.put("profileListId", profileListId);
-        ResponseEntity<VacationPeriodDto[]> vacationPeriodResponse = restTemplate.getForEntity(url, VacationPeriodDto[].class, params);
+        ResponseEntity<List<VacationPeriodDto>> vacationPeriodResponse = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {}, params);
         if (vacationPeriodResponse.getStatusCode() != HttpStatus.OK) {
             log.warn("Bad response from vacation service");
-            throw new VacationsCantReceivedException();
+            throw new VacationsReceiveException();
         } else {
-            vacationPeriods = List.of(Objects.requireNonNull(vacationPeriodResponse.getBody()));
+            vacationPeriods = Objects.requireNonNull(vacationPeriodResponse.getBody());
         }
         return vacationPeriods;
     }
